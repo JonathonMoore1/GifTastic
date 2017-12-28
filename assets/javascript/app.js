035c16ba8f140496af279b5b496a097e366295d8
JonathonMoore1 <30606521+JonathonMoore1@users.noreply.github.com>
2017-12-28 16:52:42 -0600

    Buttons generated with jQuery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,11 @@ $(document).ready(function() {
 
 	var topics = ["friend", "mother", "grandparent", "foe", "chef", "pianist", "programmer", "nemesis", "archduke", "lord", "worker", "slob", "migrant", "voyeur", "monk", "father", "companion", "competitor", "boss", "astrophysicist", "listener", "tourist", "patriot", "curmudgeon", "drunk", "hero", "rabblerouser", "dotard"];
 
-
+	//========================================================
+	//Generates a button for each of the strings in arr topics
+	//========================================================
 	$.each(topics, function(i, val) {
-		$('#human-buttons').append('<button>');
-		/*$('button').each(function() {
-			$(this).text(topics[i]);
-		});*/
-		$('button').append(val + '');
+		$('#human-buttons').append('<button>' + val);
 		console.log(this);
 	});
 
