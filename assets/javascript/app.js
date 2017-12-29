816ccc50f3ad1e0f0f0a501290b9ef9f4fb6366e
JonathonMoore1 <30606521+JonathonMoore1@users.noreply.github.com>
2017-12-29 16:48:09 -0600

    Submit button now generates a new human-button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 	var topics = ["friend", "mother", "grandparent", "foe", "chef", "pianist", "programmer", "nemesis", "archduke", "lord", "worker", "slob", "migrant", "voyeur", "monk", "father", "companion", "competitor", "boss", "astrophysicist", "listener", "tourist", "patriot", "curmudgeon", "drunk", "hero", "rabblerouser", "dotard"];
-
+	
 	//========================================================
 	//Generates a button for each of the strings in arr topics
 	//========================================================
@@ -10,6 +10,19 @@ $(document).ready(function() {
 		console.log(this);
 	});
 
-	
 
+	//========================================
+	//Generates new button based on user input
+	//========================================
+	$('#add-human').on('click', function(event) {
+		var newBtnVal = $('#human-input').val();
+		$('#human-buttons').append('<button>' + newBtnVal + '</button>');
+		event.preventDefault();
+	});
+
+
+	//=========================
+	//API links and ajax set-up
+	//=========================
+	
 });
